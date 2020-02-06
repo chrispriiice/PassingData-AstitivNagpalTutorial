cabaaa9a36b0a830345fb3191852418e3f0786ff
Chris Price <christopher.r.price@gmail.com>
2020-02-06 15:21:35 -0500

    Completed project.

--- PassingData-AstitvNagpalTutorial/FirstViewController.swift
+++ PassingData-AstitvNagpalTutorial/FirstViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, MyDataSendingDelegateProtocol {
+
+    @IBOutlet weak var receivedDataLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func sendDataToFirstViewController(myData: String) {
+        self.receivedDataLabel.text = myData
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "getDataSegue" {
+            let secondVC: SecondViewController = segue.destination as! SecondViewController
+            secondVC.delegate = self
+        }
+    }
 }
 
--- PassingData-AstitvNagpalTutorial/SecondViewController.swift
+++ PassingData-AstitvNagpalTutorial/SecondViewController.swift
@@ -8,9 +8,25 @@
 
 import UIKit
 
+protocol MyDataSendingDelegateProtocol {
+    func sendDataToFirstViewController(myData: String)
+}
+
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var dataToSendTextField: UITextField!
+    
+    var delegate: MyDataSendingDelegateProtocol? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func sendDataButtonClicked(_ sender: Any) {
+        if self.delegate != nil && self.dataToSendTextField.text != nil {
+            let dataToBeSent = self.dataToSendTextField.text
+            self.delegate?.sendDataToFirstViewController(myData: dataToBeSent!)
+            dismiss(animated: true, completion: nil)
+        }
     }
 }
